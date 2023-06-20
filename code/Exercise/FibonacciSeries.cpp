b6dc8e5ff9a98806717050de2188e63f81afb5bb
maddyCoder <103992158+maddyyyyyyy6@users.noreply.github.com>
2023-06-20 16:53:10 +0000

    fibonacci series sample code

--- code/Exercise/FibonacciSeries.cpp
+++ code/Exercise/FibonacciSeries.cpp
@@ -1,27 +1,37 @@
 #include <iostream>
 using namespace std;
 
+void getFibonacciSeries(int n);
 int main()
 {
     // to print fibonacci series
-    int current = 5, previous, preprevious;
-
-    for (int i = 1; i < 20; i++)
-    {
-        if (i >= 1)
-        {
-
-            preprevious = current - 2;
-            previous = current - 1;
-            current = previous + preprevious;
-            cout << current << " ";
-        }
-        else
-        {
-            cout << "0 ";
-        }
-
-        // cout << i - 1 << " " << i - 2 << " " << i << endl;
-    }
+    getFibonacciSeries(20);
     return 0;
+}
+
+void getFibonacciSeries(int n)
+{
+    // take n as limit
+    int t1 = 0, t2 = 1, next = 0;
+
+    for (int i = 1; i < n; i++)
+    {
+
+        if (i == 1)
+        {
+            cout << t1 << " , ";
+            continue;
+        }
+        else if (i == 2)
+        {
+            cout << t2 << " , ";
+            continue;
+        }
+        next = t1 + t2;
+        t1 = t2;
+        t2 = next;
+        cout << next << " , ";
+    }
+
+    // 0 0 1 2 3 5 8 13 21 ....
 }
