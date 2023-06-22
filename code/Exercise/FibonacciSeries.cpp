1bfd08c3f1c12542e63328e9de15832ab5dbbd60
maddyCoder <103992158+maddyyyyyyy6@users.noreply.github.com>
2023-06-22 06:29:46 +0000

    made if func

--- code/Exercise/FibonacciSeries.cpp
+++ code/Exercise/FibonacciSeries.cpp
@@ -1,37 +1,15 @@
 #include <iostream>
 using namespace std;
-
-void getFibonacciSeries(int n);
-int main()
+void main()
 {
-    // to print fibonacci series
-    getFibonacciSeries(20);
-    return 0;
-}
+    int x = 0, y = 1, cur;
 
-void getFibonacciSeries(int n)
-{
-    // take n as limit
-    int t1 = 0, t2 = 1, next = 0;
-
-    for (int i = 1; i < n; i++)
+    for (int i = 1; i < 100; i++)
     {
-
-        if (i == 1)
+        if (i == 0)
         {
-            cout << t1 << " , ";
+            cout << t1 << " ";
             continue;
         }
-        else if (i == 2)
-        {
-            cout << t2 << " , ";
-            continue;
-        }
-        next = t1 + t2;
-        t1 = t2;
-        t2 = next;
-        cout << next << " , ";
     }
-
-    // 0 0 1 2 3 5 8 13 21 ....
 }
