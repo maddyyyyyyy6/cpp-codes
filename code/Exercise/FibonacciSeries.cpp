a3c830f9d95b4de7968ff3b4c52fdb35853fdce6
maddyCoder <103992158+maddyyyyyyy6@users.noreply.github.com>
2023-06-22 06:30:39 +0000

    else add

--- code/Exercise/FibonacciSeries.cpp
+++ code/Exercise/FibonacciSeries.cpp
@@ -2,14 +2,20 @@
 using namespace std;
 void main()
 {
-    int x = 0, y = 1, cur;
+    int t1 = 0, t2 = 1, nextTerm = 0;
 
     for (int i = 1; i < 100; i++)
     {
-        if (i == 0)
+        if (i == 1)
         {
             cout << t1 << " ";
             continue;
         }
+        else if (i == 2)
+        {
+            cout << t2 << " ";
+            continue;
+        }
+        
     }
 }
