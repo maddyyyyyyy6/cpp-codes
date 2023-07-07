1342f39d4f57ffb7b04a2301d6e5394f0991d8ba
maddyCoder <103992158+maddyyyyyyy6@users.noreply.github.com>
2023-07-07 09:33:47 +0530

    Update Addition.cpp
    multiplication done

--- code/Exercise/Addition.cpp
+++ code/Exercise/Addition.cpp
@@ -34,6 +34,13 @@ void substraction()
 void mulitiplication()
 {
   float a , b , c;
+  cout << "Enter the value for first number: ";
+  cin >> a;
+  cout << "Enter the value for second number: ";
+  cin >> b;
+
+  c = a  * b;
+  cout << "Multiplication of "<< a << " and " << b<< " is " << c;
 }
 
   
