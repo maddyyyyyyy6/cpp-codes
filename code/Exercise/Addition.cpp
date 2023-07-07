7c087d24b3a62bd75b1e0dac92348e753e22650a
maddyCoder <103992158+maddyyyyyyy6@users.noreply.github.com>
2023-07-07 09:28:59 +0530

    Create Addition.cpp
    simple two float addition code written in c++

---
+++ code/Exercise/Addition.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  float a,b,c;
+  cout << "Enter the value for first number: ";
+  cin >> a;
+  cout << "Enter the value for second number: ";
+  cin >> b;
+
+  c = a +b;
+  cout << "Addition of "<< a << " and " << b<< " is " << c;
+  return 0;
+}
