dd444578d75ba6593f49faaf451e95d3ad9561cf
maddyCoder <103992158+maddyyyyyyy6@users.noreply.github.com>
2023-07-11 05:03:33 +0000

    bubble sort basic done

---
+++ code/Sort/BubbleSortAlgo.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // bubble sort - sorting the elements of a array in a ascending order
+    int a[5] = {4, 3, 2, 1, 5};
+    // int a[5] = {4, 1, 2, 3, 5};
+
+    int current = 0, next = 1, tmp;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << a[i];
+        if (a[current] > a[next])
+        {
+            cout << "  Bigger ";
+            tmp = a[next];
+            a[next] = a[current];
+            a[current] = tmp;
+        }
+        current = i;
+        next = i + 1;
+        cout << endl;
+    }
+    cout << "After sorting: " << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << a[i] << endl;
+    }
+
+    return 0;
+}
