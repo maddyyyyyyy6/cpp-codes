0de14d4f56bd6d58e2b4b9a4cf2fbb963221b531
maddyCoder <103992158+maddyyyyyyy6@users.noreply.github.com>
2023-07-11 01:58:49 +0000

    static member done

--- code/Exercise/StaticMembers.cpp
+++ code/Exercise/StaticMembers.cpp
@@ -1,35 +1,40 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
 class Student
 {
-    private:
-        static int count;
-        int roll_no;
-        int dateofbirth;
-    public:
-        Student(){};
-        Student(int rn,int dob)
-        {
-            roll_no = rn;
-            dateofbirth = dob;
-            updateCount();
+private:
+    static int count;
+    int roll_no;
+    int dateofbirth;
 
-        }
-        static void updateCount()
-        {
-            ++count;
-        }
-        static void getCount()
-        {
-            cout << "Total No. of Students Objects is: " << count;
-        }
-
+public:
+    Student(){};
+    Student(int rn, int dob=0)
+    {
+        roll_no = rn;
+        dateofbirth = dob;
+        updateCount();
+    }
+    static void updateCount()
+    {
+        ++count;
+    }
+    static void getCount()
+    {
+        cout << "Total No. of Students Objects is: " << count;
+    }
 };
 
 int main()
 {
     cout << "This is the main function";
+    // Create Student Objects
+    Student one(18);
+    Student two(19);
+    // Now every time the obj is created the count is incremented
+    Student::getCount();
+    // from this function we will get the count value
     return 0;
 }
