ab0dafbcc144f9eb725cb2331bf978ff1ccd88b7
maddyCoder <103992158+maddyyyyyyy6@users.noreply.github.com>
2023-07-11 05:48:40 +0000

    making a while in while concept

--- code/Sort/BubbleSortAlgo.cpp
+++ code/Sort/BubbleSortAlgo.cpp
@@ -7,42 +7,46 @@ int main()
     int a[10] = {412, 334, 212, 123, 4512, 3233, 142, 324, 111, 52};
     // int a[5] = {4, 1, 2, 3, 5};
 
-    int current = 0, next = 1, tmp, i = 0, e = 0;
-    while (true)
+    int current = 0, next = 1, tmp, i = 0, l = 0;
+    while (l != 9)
     {
-        cout << "Bubble Sort - round " << e;
-        // cout << a[i];
-        // cout << " current -> " << current << endl;
-        // cout << " next -> " << next << endl;
-        if (a[current] > a[next])
-        {
-            tmp = a[next];
-            a[next] = a[current];
-            a[current] = tmp;
-        }
-        current = i + 1;
-        next = i + 2;
-        cout << endl;
-        if (current == 9)
-        {
-            cout << "After sorting: " << endl;
 
-            for (int i = 0; i < 10; i++)
+        while (true)
+        {
+            cout << "Bubble Sort - round " << l;
+            // cout << a[i];
+            // cout << " current -> " << current << endl;
+            // cout << " next -> " << next << endl;
+            if (a[current] > a[next])
             {
-                cout << a[i] << endl;
+                tmp = a[next];
+                a[next] = a[current];
+                a[current] = tmp;
             }
-            i = 0;
-            current = 0;
-            next = 1;
-            cout << "New loop!" << endl;
+            current = i + 1;
+            next = i + 2;
+            cout << endl;
+            if (current == 9)
+            {
+                cout << "After sorting: " << endl;
+
+                for (int i = 0; i < 10; i++)
+                {
+                    cout << a[i] << endl;
+                }
+                i = 0;
+                current = 0;
+                next = 1;
+                cout << "New loop!" << endl;
+            }
+            if (current)
+                i++;
         }
-        if (e == 100)
+        if (l == 9)
         {
             break;
         }
-        if (current)
-            i++;
-        e++;
+        l++;
     }
     cout << "After sorting: " << endl;
     for (int i = 0; i < 10; i++)
