009552cd1cf3699fb0709159875dd13f578b5bfb
maddyCoder <103992158+maddyyyyyyy6@users.noreply.github.com>
2023-07-11 01:54:14 +0000

    Made Student Class

--- code/Exercise/StaticMembers.cpp
+++ code/Exercise/StaticMembers.cpp
@@ -1,1 +1,28 @@
+#include<iostream>
+
+using namespace std;
+
 class Student
+{
+    private:
+        static int count;
+        int roll_no;
+        int dateofbirth;
+    public:
+        Student(){};
+        Student(int rn,int dob)
+        {
+            roll_no = rn;
+            dateofbirth = dob;
+
+        }
+        static void updateCount()
+        {
+            ++count;
+        }
+        static void getCount()
+        {
+            cout << "Total No. of Students Objects is: " << count;
+        }
+
+}
