50562d2f092e1f249d06b302c84b7035891abdbf
maddyCoder <103992158+maddyyyyyyy6@users.noreply.github.com>
2023-07-15 13:44:40 +0000

    Linear search done

--- code/Algorithms/Searching/LinearSearch.cpp
+++ code/Algorithms/Searching/LinearSearch.cpp
@@ -1,11 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
     // variable for Array
     // for n value to find
-    int a[10] = {12,23,45,67,89,14,1637,96,56,34};
+    int a[10] = {12, 23, 45, 67, 89, 14, 1637, 96, 56, 34};
     int n = 2;
+    int l;
+    bool isFound = false;
+
+    for (int i = 0; i < 10; i++)
+    {
+        if (n == a[i])
+        {
+            isFound = true;
+            l = i;
+        }
+    }
+    isFound ? cout << "n is found at: " << l : cout << "n does not exist!";
     return 0;
 }
