1b2b76716a2bd25f7ec540b1a5dcce4000383cc3
maddyCoder <103992158+maddyyyyyyy6@users.noreply.github.com>
2023-07-24 16:44:36 +0000

    11 asg done

--- code/Assignments/Asg11_FactorialClass.cpp
+++ code/Assignments/Asg11_FactorialClass.cpp
@@ -12,6 +12,17 @@ class Factorial
              this is a function
         }
 
+        void getFactorial(int n)
+        {
+            int t = n;
+            int t1 = 0,t2=1,nexterm = 0;
+
+            for(int i = t-1;i>= 1; i--)
+            {
+                
+            }
+        }
+
 }
 
 int main()
