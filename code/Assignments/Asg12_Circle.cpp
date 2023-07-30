78b86914fae170ba094eefbea7d8a0418c216798
maddyCoder <103992158+maddyyyyyyy6@users.noreply.github.com>
2023-07-30 18:38:46 +0530

    Update Asg12_Circle.cpp

--- code/Assignments/Asg12_Circle.cpp
+++ code/Assignments/Asg12_Circle.cpp
@@ -47,3 +47,49 @@ public:
     c.circum();
     return 0;
 }
+
+
+
+
+
+
+
+// new code for assignment with corrections
+
+#include <iostream>
+using namespace std;
+
+class Circle
+{
+private:
+    float pi = 3.14;
+    float Area;
+    float radius;
+    float circumference;
+
+public:
+    void getRadius()
+    {
+        cout << "Enter value for radius: ";
+        cin >> radius;
+    }
+
+    void area()
+    {
+        Area = pi * radius * radius;
+        cout << "Area of the circle with radius: " << radius << " is " << Area << endl;
+    }
+    void circum()
+    {
+        circumference = 2 * pi * radius;
+        cout << "Circumference of the circle with radius: " << radius << " is " << circumference << endl;
+    }
+};
+int main()
+{
+    Circle c;
+    c.getRadius();
+    c.area();
+    c.circum();
+    return 0;
+}
