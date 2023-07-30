1ca968d5c3ac66eaa5c68fe03bf60effe5d0ba31
maddyCoder <103992158+maddyyyyyyy6@users.noreply.github.com>
2023-07-30 18:13:47 +0530

    Update Asg10_GCD.cpp

--- code/Assignments/Asg10_GCD.cpp
+++ code/Assignments/Asg10_GCD.cpp
@@ -1,24 +1,42 @@
 #include <iostream>
 using namespace std;
 
+class GCD
+{
+    private:
+        int x,y,gcd;
+    public:
+        void getValues()
+        {
+            cout << "Enter the value of two Numbers: ":
+            cin >> x >> y;
+        }
+        void getGCD()
+        {
+			
+            n = (x < y) ? x : y;
+            for (int i = 2; i <= n; i++)
+            {
+                if (x % i == 0 && y % i == 0)
+                {
+                    gcd = i;
+                    // cout << "This is the GCD of both " << i << endl;
+                }
+                else
+                {
+					// do nothing
+                }
+            }
+			cout << "GCD of" << x << " and " << y << " is "<< gcd;
+		}
+}
+            
+
 int main()
 {
-    int a = 5, b = 10, n, gcd;
-    cout << "Enter two value for GCDs: ";
-    cin >> a >> b;
-    n = (a < b) ? a : b;
+    GCD g;
+	g.getValues();
+	g.getGCD();
 
-    for (int i = 2; i <= n; i++)
-    {
-        if (a % i == 0 && b % i == 0)
-        {
-            gcd = i;
-            // cout << "This is the GCD of both " << i << endl;
-        }
-        else
-        {
-        }
-    }
-    cout << "GCD : " << gcd << endl;
     return 0;
 }
